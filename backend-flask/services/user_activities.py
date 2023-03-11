66c527393b3a12bc51925499436150ee3ab2869f
Oluwajuwon <juwon4real007@gmail.com>
2023-03-11 07:53:20 +0000

    Implement custom span to capture UserID

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,17 +1,26 @@
 from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+
+tracer = trace.get_tracer("user-activities")
+
 class UserActivities:
   def run(user_handle):
+    with tracer.start_as_current_span("user-data-activities"): # start a new custom spam called user-data
+     span = trace.get_current_span()
     model = {
       'errors': None,
       'data': None
     }
 
     now = datetime.now(timezone.utc).astimezone()
+    span.set_attribute("user.now", now.isoformat())
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
       now = datetime.now()
+      span.set_attribute("user.now", now.isoformat()) # capture the time
+      span.set_attribute("UserID", user_handle) # capture the user id
       results = [{
         'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
         'handle':  'AJLarry',
@@ -19,5 +28,6 @@ class UserActivities:
         'created_at': (now - timedelta(days=1)).isoformat(),
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
+      span.set_attribute("user.activities.len", len(results)) # capture the number of active user activities 
       model['data'] = results
     return model
